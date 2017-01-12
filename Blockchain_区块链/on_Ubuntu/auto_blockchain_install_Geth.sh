b2dc79d5b4910d9608d6c797ce00cb9ed0164918
adamhuan <dkseven_eli@163.com>
2017-01-12 13:21:30 +0800

    Blockchain，区块链（以太坊GETH，on UBUNTU Linux）
    在Ubuntu Linux上部署区块链（以太坊）。

---
+++ Blockchain_区块链/on_Ubuntu/auto_blockchain_install_Geth.sh
@@ -0,0 +1,6 @@
+apt-get update
+apt-get install -y software-properties-common
+add-apt-repository -y ppa:ethereum/ethereum
+add-apt-repository -y ppa:ethereum/ethereum-dev
+apt-get update
+apt-get install -y ethereum
