98258ad7e3428e44aafac437fe29ee14a95f38b9
adamhuan <dkseven_eli@163.com>
2017-01-12 12:09:47 +0800

    Ubuntu_自动化脚本
    自动为Ubuntu安装图形化界面（GUI）。

---
+++ Ubuntu/图形界面/自动部署图形界面/auto_ubuntu_linux_GUI_desktop_install.sh
@@ -0,0 +1,12 @@
+apt-get install ubuntu-desktop
+apt-get install unity
+apt-get install unity-common
+apt-get install unity-lens*
+apt-get install unity-services
+apt-get install unity-asset-pool
+
+apt-get update
+apt-get upgrade
+
+apt-get autoremove
+apt-get autoclean
