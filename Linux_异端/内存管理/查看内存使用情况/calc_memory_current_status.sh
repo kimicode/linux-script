ca68cf84eac6ea06af7abf09b88fe67a8fd8ee57
adamhuan <dkseven_eli@163.com>
2017-03-03 17:31:36 +0800

    更新，RHEL7与RHEL6中的差异。

--- Linux_异端/内存管理/查看内存使用情况/calc_memory_current_status.sh
+++ Linux_异端/内存管理/查看内存使用情况/calc_memory_current_status.sh
@@ -1,7 +1,12 @@
 # calc_memory_current_stat.sh
 
+#for_RHEL7="Available"
+#for_RHEL6="Free"
+
+Available_sign="Free"
+
 mem_total=`cat /proc/meminfo  | grep --color MemTotal | awk '{print $2}'`
-mem_available=`cat /proc/meminfo  | grep --color MemAvailable | awk '{print $2}'`
+mem_available=`cat /proc/meminfo  | grep --color Mem$Available_sign | awk '{print $2}'`
 
 compute_percent_mem_available=`awk 'BEGIN {printf "%.4f\n",(('$mem_available'/'$mem_total')*100)}'`
 compute_percent_mem_used=`awk 'BEGIN {printf "%.4f\n",((1-'$mem_available'/'$mem_total')*100)}'`
