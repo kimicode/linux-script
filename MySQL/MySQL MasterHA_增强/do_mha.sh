9f2098c8de5f1b1bcc6b103d4719a0b399278ae3
adamhuan <dkseven_eli@163.com>
2017-03-07 00:42:13 +0800

    更新了do_mha.sh没有考虑到的不同环境中MySQL端口也可能不一样的情况，增加了自定义处理VIP资源的逻辑，并包含只希望在从主节点上运行计划任务的诉求（为了实现这个功能，修改了ip_change_online与ip_failover脚本，所以如果需要使用，它们需要一起被更新）

--- MySQL/MySQL MasterHA_增强/do_mha.sh
+++ MySQL/MySQL MasterHA_增强/do_mha.sh
@@ -17,15 +17,15 @@ str_mha_application=$1
 
 # Part:: Linux
 # Account info
-str_linux_username="root"
+str_linux_username="root"       #不同的环境，一定要注意的参数
 
 # Part:: MySQL
 # Account info
-str_mysql_username="root"
-str_mysql_password=Abcd1@34
+str_mysql_username="root"       #不同的环境，一定要注意的参数
+str_mysql_password=Abcd1@34     #不同的环境，一定要注意的参数
 
-str_repl_username="replme"
-str_repl_password=Or@cle123
+str_repl_username="replme"      #不同的环境，一定要注意的参数
+str_repl_password=Or@cle123     #不同的环境，一定要注意的参数
 
 # Part:: MHA
 # pid
@@ -36,8 +36,10 @@ str_pid_masterha_manager=`ps -ef | grep masterha_manager | grep "$str_mha_applic
 # file
 file_conf_mha_global="/etc/masterha_default.cnf"
 
+path_conf_mha_application="/etc"    #不同的环境，一定要注意的参数
+
 #file_conf_mha_application="/etc/masterha_application_1.cnf"
-file_conf_mha_application="/etc/$str_mha_application.cnf"
+file_conf_mha_application="$path_conf_mha_application/$str_mha_application.cnf"   #不同的环境，一定要注意的参数
 
 # file: relation / by computed
 file_log_mha_manager=`cat $file_conf_mha_application | grep --color manager_log | cut -d'=' -f2`
@@ -49,7 +51,11 @@ list_ip_candicate=`cat $file_conf_mha_application | grep -B 2 "^candidate" | gre
 str_ip_orig_master=`cat $file_log_mha_manager | grep --color "MySQL Master failover" | cut -d'(' -f2 | cut -d':' -f1 | tail -n 1`
 str_ip_new_master=`cat $file_log_mha_manager | grep --color "MySQL Master failover" | cut -d'(' -f3 | cut -d':' -f1 | tail -n 1`
 
-str_ip_mha_manager="10.158.1.94"
+str_ip_mha_manager="10.158.1.94"    #不同的环境，一定要注意的参数
+str_mysql_port="3306"               #不同的环境，一定要注意的参数
+
+path_file_mha_application_log_dir="/script/log"     #不同的环境，一定要注意的参数
+path_file_mha_application_log_file="$path_file_mha_application_log_dir/$str_mha_application.log"
 
 # 为[change master]准备的参数
 str_log_file_new_master=""
@@ -69,7 +75,7 @@ function do_sql() {
   # action
   # 本场景中不涉及到对MySQL某个库的操作，所以没有选择[db]
   # mysql -u $user -p"$password" $db -N -e "$f_sql_str"
-  mysql -u $str_mysql_username -h $func_str_ip -p"$str_mysql_password" -N -e "$func_str_sql"
+  mysql -u $str_mysql_username -h $func_str_ip -p"$str_mysql_password" -P$str_mysql_port -N -e "$func_str_sql"
 }
 
 # 获取主库状态信息
@@ -92,9 +98,20 @@ function gen_sql_mysql_change_master() {
   func_temp_master_host_sed=`cat $file_log_mha_manager | grep --color "All other slaves should start" | tail -n 1 | cut -d',' -f1 | cut -d'=' -f2 | cut -d\' -f2`
   func_temp_repl_password_sed=`cat $file_log_mha_manager | grep --color "All other slaves should start" | tail -n 1 | rev | cut -d\' -f2`
 
+  func_temp_master_binlog_file=`cat $file_log_mha_manager | grep --color "All other slaves should start" | tail -n 1 | cut -d',' -f3 | cut -d'=' -f2 | cut -d\' -f2`
+  func_temp_master_binlog_pos=`cat $file_log_mha_manager | grep --color "All other slaves should start" | tail -n 1 | cut -d',' -f4 | cut -d'=' -f2 | cut -d\' -f2`
+
   echo "======================"
+  echo "@@ func variable: func_temp_master_host_sed = $func_temp_master_host_sed"
   echo "@@ func variable: func_temp_repl_password_sed = $func_temp_repl_password_sed"
+
+  echo "@@ func variable: func_temp_master_binlog_file = $func_temp_master_binlog_file"
+  echo "@@ func variable: func_temp_master_binlog_pos = $func_temp_master_binlog_pos"
   echo "======================"
+
+  str_log_file_new_master=$func_temp_master_binlog_file
+  str_log_pos_new_master=$func_temp_master_binlog_pos
+
   str_sql_mysql_change_master=`cat $file_log_mha_manager | grep --color "All other slaves should start" | tail -n 1 | sed "s/'$func_temp_repl_password_sed'/'$str_repl_password'/g" | cut -d':' -f4`
   str_sql_mysql_change_master=`echo $str_sql_mysql_change_master | sed "s/'$func_temp_master_host_sed'/'$str_ip_new_master'/g"`
 }
@@ -127,7 +144,31 @@ function do_part_orig_master_is_new_slave() {
 }
 
 function do_part_mha_master_manager_start() {
-  do_linux_by_ssh "$str_ip_mha_manager" "root" "nohup masterha_manager --conf=$file_conf_mha_application --ignore_last_failover &"
+  echo "func: do_part_mha_master_manager_start --->"
+  echo "Special log file is: $path_file_mha_application_log_file"
+  do_linux_by_ssh "$str_ip_mha_manager" "root" "nohup masterha_manager --conf=$file_conf_mha_application --ignore_last_failover < /dev/null > $path_file_mha_application_log_file 2>&1 &"
+}
+
+function check_repl() {
+  # variable
+  func_str_block_status=""
+
+  # version 1
+  #func_str_block_status=`masterha_check_repl --conf=$file_conf_mha_application`
+
+  # version 2
+  #masterha_check_repl --conf=$file_conf_mha_application > $func_str_block_status
+
+  # version 3
+  func_str_block_status=$(masterha_check_repl --conf=$file_conf_mha_application)
+
+  repl_status=`echo "$func_str_block_status" | grep --color "(current master)"`
+
+  echo "------------------"
+  echo "fun: check_repl"
+  echo ""
+  echo "$func_str_block_status"
+  echo ""
 }
 
 # 如果PID不存在，则执行该脚本，否则，退出
@@ -136,6 +177,11 @@ function runable_by_mha_manager_pid() {
   echo "Script for MySQL Master HA"
   echo "-----------------"
   echo "Begin:: "`date "+|%Y-%m-%d|%H:%M:%S|"`
+
+  #echo "========================"
+  #echo "current MySQL REPL is::"
+  #check_repl
+  #echo "========================"
 
   if [[ "$str_pid_masterha_manager" == "" ]]
   then
@@ -176,7 +222,13 @@ echo ""
 echo "------------------"
 echo "app: do_part_mha_master_manager_start"
 #do_part_mha_master_manager_start
-nohup masterha_manager --conf=$file_conf_mha_application --ignore_last_failover &
+# version 1
+#nohup masterha_manager --conf=$file_conf_mha_application --ignore_last_failover &
+
+# version 2
+echo "Special log file is: $path_file_mha_application_log_file"
+nohup masterha_manager --conf=$file_conf_mha_application --ignore_last_failover < /dev/null >> $path_file_mha_application_log_file 2>&1 &
+
 echo ""
 
 # --------------------------
@@ -199,12 +251,21 @@ echo "MySQL info:"
 echo "## Account and Password"
 echo "username @ $str_mysql_username"
 echo "password @ $str_mysql_password"
+echo ""
 echo "--- for REPLICATION ---"
 echo "repl @ username ## $str_repl_username"
 echo "repl @ password ## $str_repl_password"
+echo ""
 echo "## Master Server info"
+
+#version 1
+#echo "log file @ Master ## $str_log_file_new_master"
+#echo "log pos  @ Master ## $str_log_pos_new_master"
+
+#version 2
 echo "log file @ Master ## $str_log_file_new_master"
 echo "log pos  @ Master ## $str_log_pos_new_master"
+
 echo ""
 
 echo "================="
@@ -220,11 +281,11 @@ echo "## File and Path"
 echo "MHA Global config file @ $file_conf_mha_global"
 echo "MHA Application config file @ $file_conf_mha_application"
 echo "MHA Log file:: masterha_manager @ $file_log_mha_manager"
-
+echo ""
 echo "## Architecture"
 echo "Candicate Server list::"
 echo "$list_ip_candicate"
-
+echo ""
 echo "## IP"
 echo "MHA Manager Server:: $str_ip_mha_manager"
 echo "Last:: new master:: $str_ip_new_master"
---
+++ MySQL/MySQL MasterHA_增强/do_vip_and_crontab.sh
@@ -0,0 +1,106 @@
+# file: do_vip_and_crontab.sh
+
+# 职能：
+# --> 1. 启动或停止：虚拟IP
+# --> 2. 启用或禁用：crontab策略
+
+# variable
+
+# ---> 信号量
+# 对于所有信号而言：
+# 0，启用
+# 1，禁用
+
+#是否启用：VIP
+#sign_vip=0
+sign_vip="$1"
+
+# 是否启用：crontab
+#sign_crontab=0
+sign_crontab="$2"
+
+# 对于VIP而言
+str_vip="10.158.1.111"
+str_mask="255.255.255.0"
+str_nic_name="eth0:0"
+
+# 对于CRONTAB而言
+
+# for RHEL6
+# 如果是root用户，就会在：/var/spool/cron/root
+file_crontab_dir="/var/spool/cron"
+file_crontab_conf="$file_crontab_dir/root"
+
+str_crontab_command="data +%Y-%m-%d"
+str_crontab_date="10 11 * * *"
+
+str_crontab_date_sed=`echo "$str_crontab_date" | sed 's/*/\\\*/g'`
+
+str_crontab_full="$str_crontab_date $str_crontab_command"
+str_crontab_full_sed="$str_crontab_date_sed $str_crontab_command"
+
+str_block_crontab_content=`cat $file_crontab_conf | grep -v '#'`
+
+# disable
+
+echo "Crontab content:"
+echo "$str_block_crontab_content"
+echo ""
+
+echo "var: str_crontab_full --> $str_crontab_full"
+echo "var: str_crontab_date_sed --> $str_crontab_date_sed"
+echo "var: str_crontab_full_sed --> $str_crontab_full_sed"
+echo ""
+
+# running
+
+if [ $sign_vip -eq "0" ]
+then
+  echo "------------"
+  echo "VIP:: enable."
+  ifconfig $str_nic_name $str_vip netmask $str_mask up
+  echo ""
+else
+  echo "------------"
+  echo "VIP:: disable."
+  ifconfig $str_nic_name $str_vip netmask $str_mask down
+  echo ""
+fi
+
+if [ $sign_crontab -eq "0" ]
+then
+  echo "------------"
+  echo "CRONTAB:: enable."
+
+  if [[ $str_block_crontab_content =~ "$str_crontab_full" ]]
+  then
+    echo "CRONTAB Policy is in [$file_crontab_conf]"
+
+  else
+    echo "CRONTAB Policy is not in [$file_crontab_conf]"
+    echo "Put it in, ..."
+    echo "$str_crontab_full" >> $file_crontab_conf
+    echo "Done."
+  fi
+
+  echo ""
+else
+  echo "------------"
+  echo "CRONTAB:: disable."
+
+  if [[ $str_block_crontab_content =~ "$str_crontab_full" ]]
+  then
+    echo "CRONTAB Policy is in [$file_crontab_conf]"
+    echo "change it, ..."
+    sed -i "s/$str_crontab_full_sed/#$str_crontab_full_sed/g" $file_crontab_conf
+    echo "Done."
+
+  else
+    echo "CRONTAB Policy is not in [$file_crontab_conf]"
+
+  fi
+
+  echo ""
+fi
+
+# Finished
