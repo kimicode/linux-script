3c36491569e75fa16940398c91fed4dcbc0bf256
adamhuan <dkseven_eli@163.com>
2017-03-10 23:59:42 +0800

    一些新的尝试。

--- snick_精刻之痕/logical_structure/do_while.sh
+++ snick_精刻之痕/logical_structure/do_while.sh
@@ -4,13 +4,25 @@ list_me=""
 
 cursor_me="1"
 
-while [ $cursor_me -lt 10 ]
-do
-  list_me="$list_me $cursor_me"
-  let "cursor_me++"
-done
+function get_value(){
+  while [ $cursor_me -lt 10 ]
+  do
+    list_me="$list_me $cursor_me"
+    let "cursor_me++"
+  done
 
-echo "# list is:"
+}
+
+#get_value
+
+echo "# list [before] is:"
+echo "$list_me"
+
+echo ""
+
+get_value
+
+echo "# list [after] is:"
 echo "$list_me"
 
 # finished
